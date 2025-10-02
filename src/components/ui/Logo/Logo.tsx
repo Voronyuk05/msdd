36e6ae572eabb00bfa6a1ba7005a14d509eca88b
Ivan <vandoslando@gmail.com>
2025-10-02 13:35:48 +0200

    file v1.37

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -9,12 +9,12 @@ export default function Logo() {
         <div className={styles.logo}>
             <div className={styles.logo_item}>
                 <Image src={ukrainian_flag} alt="флаг України" priority width={50} height={37} />
-                <Headings color="secondary" heading="h5" weight="700">Міжнародний Спеціалізований Департамент Детективів</Headings>
+                <Headings color="secondary" heading="h6" weight="700">Міжнародний Спеціалізований Департамент Детективів</Headings>
             </div>
             <div className={styles.line}></div>
             <div className={styles.logo_item}>
                 <Image src={european_flag} alt='флаг Європи' priority width={50} height={37}/>
-                <Headings color='primary' heading='h5' weight='700'>International Specialized Department Of Detectives</Headings>
+                <Headings color='primary' heading='h6' weight='700'>Département International Spécialisé Dans Les Enquêtes Privées</Headings>
             </div>
         </div>
     )
