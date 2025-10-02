22990c9c136a5a742114c67637a5fbda13a2b1a8
Ivan <vandoslando@gmail.com>
2025-10-02 13:31:00 +0200

    file v1.36

--- src/components/ui/Logo/Logo.tsx
+++ src/components/ui/Logo/Logo.tsx
@@ -1,13 +1,21 @@
 import { Headings } from '../Headings/Headings';
-import logo from "../../../../public/logo.png";
+import ukrainian_flag from "../../../../public/Flag_of_Ukraine.png";
+import european_flag from "../../../../public/Flag_of_Europe.png";
 import Image from "next/image";
 import styles from "./Logo.module.scss";
 
 export default function Logo() {
     return (
         <div className={styles.logo}>
-            <Image src={logo} alt="лого Міжнародного - Спеціального Департаменту Детективів" priority width={75} height={75} />
-            <Headings color="primary" heading="h1" weight="700">М-СДД</Headings>
+            <div className={styles.logo_item}>
+                <Image src={ukrainian_flag} alt="флаг України" priority width={50} height={37} />
+                <Headings color="secondary" heading="h5" weight="700">Міжнародний Спеціалізований Департамент Детективів</Headings>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.logo_item}>
+                <Image src={european_flag} alt='флаг Європи' priority width={50} height={37}/>
+                <Headings color='primary' heading='h5' weight='700'>International Specialized Department Of Detectives</Headings>
+            </div>
         </div>
     )
 }
